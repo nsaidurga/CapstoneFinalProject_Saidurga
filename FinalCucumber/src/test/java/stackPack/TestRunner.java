9e974002657e6f3cc8ccea818e10da1e6b8532b4
NARAYANAPURAM SAIDURGA <139451339+nsaidurga@users.noreply.github.com>
2025-09-08 19:37:24 +0530

    Update TestRunner.java

--- FinalCucumber/src/test/java/stackPack/TestRunner.java
+++ FinalCucumber/src/test/java/stackPack/TestRunner.java
@@ -1,16 +1,15 @@
 package stackPack;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/stackkFolder",
 				glue= {"stackPack"},
-				plugin= {"pretty","html:target/cucumber-reports/report.html",
-						"json:target/cucumber-reports/report.json",
-						"junit:target/cucumber-reports/report.xml"})
+				plugin= {"pretty","html:target/htmlreport.html",
+						"json:target/report.json",
+						"junit:target/report.xml"})
 
 public class TestRunner {
 
