4a762e06caef149137ea00f8b0263495ba422d72
NARAYANAPURAM SAIDURGA <139451339+nsaidurga@users.noreply.github.com>
2025-09-08 19:31:26 +0530

    Update FilterProducts.java

--- FinalCucumber/src/test/java/stackPack/FilterProducts.java
+++ FinalCucumber/src/test/java/stackPack/FilterProducts.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -15,20 +16,22 @@ public class FilterProducts {
 	
 	@Given("Home3 page visible")
 	public void home3_page_visible() {
-		d=new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.setPageLoadStrategy(org.openqa.selenium.PageLoadStrategy.EAGER);
+		d=new ChromeDriver(options);
 		d.get("https://bstackdemo.com/");
 		d.manage().window().maximize();
 		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 
 	@When("click on search button")
-	public void click_on_search_button() throws InterruptedException {
+	public void click_on_search_button() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.serchbut();
 	}
 
 	@Then("click on Order by select button")
-	public void click_on_order_by_select_button() throws InterruptedException {
+	public void click_on_order_by_select_button() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.clickorderby();
 	}
@@ -40,10 +43,9 @@ public class FilterProducts {
 	}
 
 	@And("Scroll the List of products")
-	public void scroll_the_list_of_products() throws InterruptedException {
+	public void scroll_the_list_of_products()  {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.scroll();
 	}
-
 
 }
