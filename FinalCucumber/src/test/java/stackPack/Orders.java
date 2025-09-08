8061c86a27bf76c601404fa73108178c4670c48b
NARAYANAPURAM SAIDURGA <139451339+nsaidurga@users.noreply.github.com>
2025-09-08 19:32:57 +0530

    Update Orders.java

--- FinalCucumber/src/test/java/stackPack/Orders.java
+++ FinalCucumber/src/test/java/stackPack/Orders.java
@@ -1,6 +1,7 @@
 package stackPack;
 
 import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -14,6 +15,7 @@ public class Orders {
 	
 	@Given("User is on the home page")
 	public void user_is_on_the_home_page() {
+		
 		d=new ChromeDriver();
 		d.get("https://bstackdemo.com/");
 		d.manage().window().maximize();
@@ -21,19 +23,19 @@ public class Orders {
 	}
 
 	@Given("Click on Orders tab")
-	public void click_on_orders_tab() throws InterruptedException {
+	public void click_on_orders_tab() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.clickonorders();
 	}
 
 	@When("existingUser login with credentials")
-	public void existing_user_login_with_credentials() throws InterruptedException {
+	public void existing_user_login_with_credentials() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.existingsuser();
 	}
 
 	@And("Clicked the order tab")
-	public void clicked_the_order_tab() throws InterruptedException {
+	public void clicked_the_order_tab() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.clickorders1();
 	}
@@ -44,59 +46,63 @@ public class Orders {
 	}
 
 	@And("scrolldown page and close")
-	public void scrolldown_page_and_close() throws InterruptedException {
+	public void scrolldown_page_and_close() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.scrollorders();
+		d.close();
 	}
 //
 	
 	@When("demoUser login with credentials")
-	public void demo_user_login_with_credentials() throws InterruptedException {
+	public void demo_user_login_with_credentials() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.demouser1();
 	}
 
 	@And("Clicked1 the order tab")
-	public void clicked1_the_order_tab() throws InterruptedException {
+	public void clicked1_the_order_tab() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.clickorders2();
 	}
 
-	@Then("added anew product")
-	public void added_anew_product() throws InterruptedException {
+	@Then("a1dded112 anew product {string}, {string}, {string}, {string} and {string}")
+	public void a1dded112_anew_product(String firstname1,String lastname1,String address1,String state1,String pincode1){
 		PageClassSDB pc=new PageClassSDB(d);
-		pc.newaddprod();
+		pc.newaddprod(firstname1, lastname1, address1, state1, pincode1);
+		System.out.println("one svssh");
 	}
 
 	@And("all placed orders")
-	public void all_placed_orders() throws InterruptedException {
+	public void all_placed_orders() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.placedorders();
+		d.close();
 	}
 //
 	
 	@When("favoriteUser login with credentials")
-	public void favorite_user_login_with_credentials() throws InterruptedException {
+	public void favorite_user_login_with_credentials() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.favouriteuser1();
 	}
 
 	@And("Clicked2 the order tab")
-	public void clicked2_the_order_tab() throws InterruptedException {
+	public void clicked2_the_order_tab() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.clickorders3();
 	}
 
-	@Then("place the order from favourites")
-	public void place_the_order_from_favourites() throws InterruptedException {
+	@Then("p1lace112 the order from favourites {string}, {string}, {string}, {string} and {string}")
+	public void p1lace112_the_order_from_favourites(String firstname,String lastname,String address,String state,String pincode) {
 		PageClassSDB pc=new PageClassSDB(d);
-		pc.placeorderfromfavs();
+		pc.placeorderfromfavs(firstname, lastname, address, state, pincode);
 	}
 
 	@And("order page opened")
-	public void order_page_opened() throws InterruptedException {
+	public void order_page_opened() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.orderpageopen();
+		d.close();
 	}
 
 }
