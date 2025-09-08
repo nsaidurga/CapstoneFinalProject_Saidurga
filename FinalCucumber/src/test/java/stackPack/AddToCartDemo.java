c8e5d8b01d675b6c57b9e66b544fac84174998b1
NARAYANAPURAM SAIDURGA <139451339+nsaidurga@users.noreply.github.com>
2025-09-08 19:28:34 +0530

    Update AddToCartDemo.java

--- FinalCucumber/src/test/java/stackPack/AddToCartDemo.java
+++ FinalCucumber/src/test/java/stackPack/AddToCartDemo.java
@@ -1,9 +1,10 @@
 package stackPack;
 
 import java.time.Duration;
-import org.openqa.selenium.WebDriver;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -14,36 +15,36 @@ public class AddToCartDemo {
 	WebDriver d;
 	@Given("Home page visible")
 	public void home_page_visible() {
-		d=new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.setPageLoadStrategy(org.openqa.selenium.PageLoadStrategy.EAGER);
+		d=new ChromeDriver(options);
 		d.get("https://bstackdemo.com/");
 		d.manage().window().maximize();
 		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 
-	@When("search a product")
-	public void search_a_product() throws InterruptedException {
+	@When("search a product {string}")
+	public void search_a_product(String proname) {
 		PageClassSDB pc=new PageClassSDB(d);
-		pc.searchprod();
+		pc.searchprod(proname);
 	}
 
 	@Then("click on addtocart button")
-	public void click_on_addtocart_button() throws InterruptedException {
+	public void click_on_addtocart_button() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.addtocartbuton();
 	}
 	
 
 	@And("increase the count for same product")
-	public void increase_the_count_for_same_product() throws InterruptedException {
+	public void increase_the_count_for_same_product() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.counter();
 	}
 
 	@And("check details of the product")
-	public void check_details_of_the_product() throws InterruptedException {
+	public void check_details_of_the_product() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.detailsofprod();
 	}
-	//(//div[@class="shelf-item__buy-btn"])[3]
-			//Navbar_logo__image__3Blki
 }
