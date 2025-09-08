b4ad1f472dfe7bdbe003ce932160eaed1aab4d9a
NARAYANAPURAM SAIDURGA <139451339+nsaidurga@users.noreply.github.com>
2025-09-08 19:36:06 +0530

    Update SelectByCategoryD.java

--- FinalCucumber/src/test/java/stackPack/SelectByCategoryD.java
+++ FinalCucumber/src/test/java/stackPack/SelectByCategoryD.java
@@ -1,6 +1,7 @@
 package stackPack;
 
 import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -14,6 +15,7 @@ public class SelectByCategoryD {
 	
 	@Given("Home2 page visible")
 	public void home2_page_visible() {
+		
 		d=new ChromeDriver();
 		d.get("https://bstackdemo.com/");
 		d.manage().window().maximize();
@@ -26,52 +28,52 @@ public class SelectByCategoryD {
 	}
 
 	@When("click on single category button")
-	public void click1_on_single_category_button() throws InterruptedException {
+	public void click1_on_single_category_button() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.singlecatclick();
 	}
 
 	@Then("See the specified selected category of the products")
-	public void see_the_specified_selected_category_of_the_products() throws InterruptedException {
+	public void see_the_specified_selected_category_of_the_products() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.singcatprods();
 	}
 
 //
 	@When("click on two category buttons")
-	public void click_on_two_category_buttons() throws InterruptedException {
+	public void click_on_two_category_buttons() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.twocatclick();
 	}
 
 	@Then("See the two specified selected categories of the products")
-	public void see_the_two_specified_selected_categories_of_the_products() throws InterruptedException {
+	public void see_the_two_specified_selected_categories_of_the_products() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.doblecatprods();
 	}
 
 //
 	@When("click on three category buttons")
-	public void click_on_three_category_buttons() throws InterruptedException {
+	public void click_on_three_category_buttons() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.threecatclick();
 	}
 
 	@Then("See the three particular selected categories of the products")
-	public void see_the_three_particular_selected_categories_of_the_products() throws InterruptedException {
+	public void see_the_three_particular_selected_categories_of_the_products() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.threecatprods();
 	}
 
 //
 	@When("click on four category buttons")
-	public void click_on_four_category_buttons() throws InterruptedException {
+	public void click_on_four_category_buttons() {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.fourcatclick();
 	}
 
 	@Then("See the four particular selected categories of the products")
-	public void see_the_four_particular_selected_categories_of_the_products() throws InterruptedException {
+	public void see_the_four_particular_selected_categories_of_the_products()  {
 		PageClassSDB pc=new PageClassSDB(d);
 		pc.fourcatprods();
 	}
